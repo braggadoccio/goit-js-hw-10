bf070a11d25a499a944abd03e46e065a47ee1afd
Paulo <pau.solanoy@gmail.com>
2023-11-23 09:03:58 +0800

    final update

--- src/index.js
+++ src/index.js
@@ -1,41 +1,36 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
-////////////////////////////////////////////////////////////
+// DOM
+const breedSelectElement = document.querySelector('.breed-select');
+const catInfoElement = document.querySelector('.cat-info');
+const loaderElement = document.querySelector('.loader');
+const errorElement = document.querySelector('.error');
 
-const breedSelectEl = document.querySelector('.breed-select');
-const catInfoEl = document.querySelector('.cat-info');
-const loaderEl = document.querySelector('.loader');
-const errorEl = document.querySelector('.error');
-
-////////////////////////////////////////////////////////////
-
-// for creating the options
+// Options
 function chooseBreed(data) {
   fetchBreeds(data)
     .then(data => {
       //   console.log(data);
-      loaderEl.classList.replace('loader', 'is-hidden');
+      loaderElement.classList.replace('loader', 'is-hidden');
 
       let optionsMarkup = data.map(({ name, id }) => {
         return `<option value ='${id}'>${name}</option>`;
       });
 
-      breedSelectEl.insertAdjacentHTML('beforeend', optionsMarkup);
-      breedSelectEl.classList.remove('is-hidden'); // Show select element after options are added
+      breedSelectElement.insertAdjacentHTML('beforeend', optionsMarkup);
+      breedSelectElement.classList.remove('is-hidden'); // Show select element after options are added
     })
     .catch(onError);
 }
 
 chooseBreed();
 
-////////////////////////////////////////////////////////////
-
 function createMarkup(event) {
   // Show loader while loading
-  loaderEl.classList.replace('is-hidden', 'loader');
+  loaderElement.classList.replace('is-hidden', 'loader');
   // Hide select element and cat info markup while loading
-  breedSelectEl.classList.add('is-hidden');
-  catInfoEl.classList.add('is-hidden');
+  breedSelectElement.classList.add('is-hidden');
+  catInfoElement.classList.add('is-hidden');
 
   const breedId = event.target.value;
   //   get the option value using event.target.value
@@ -44,13 +39,13 @@ function createMarkup(event) {
 
   fetchCatByBreed(breedId)
     .then(data => {
-      loaderEl.classList.replace('loader', 'is-hidden');
-      breedSelectEl.classList.remove('is-hidden');
+      loaderElement.classList.replace('loader', 'is-hidden');
+      breedSelectElement.classList.remove('is-hidden');
 
       const { url, breeds } = data[0];
       const { name, description, temperament } = breeds[0];
 
-      catInfoEl.innerHTML = `
+      catInfoElement.innerHTML = `
       <img src="${url}" alt="${name}" width="400"/>
       <div class="box">
         <h2>${name}</h2>
@@ -58,18 +53,16 @@ function createMarkup(event) {
         <p><strong>Temperament:</strong> ${temperament}</p>
       </div>
       `;
-      catInfoEl.classList.remove('is-hidden');
+      catInfoElement.classList.remove('is-hidden');
     })
     .catch(onError);
 }
 
-breedSelectEl.addEventListener('change', createMarkup);
-
-////////////////////////////////////////////////////////////
+breedSelectElement.addEventListener('change', createMarkup);
 
 function onError() {
   // Show error Message
-  errorEl.classList.remove('is-hidden');
+  errorElement.classList.remove('is-hidden');
   //   Hide select element
-  breedSelectEl.classList.add('is-hidden');
+  breedSelectElement.classList.add('is-hidden');
 }
